d1ef70c948e05550784a5de5e985ee61f7747b01
Serg_am <serg_am@live.ru>
2023-09-07 22:47:19 +0300

    Добавил перебор потоками

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,12 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class App {
     static String url = "jdbc:postgresql://localhost:5432/postgres";
     static String username = "postgres";
     static String password = "43760097";
     static int countDb = 0;
+
+    static AtomicInteger counter = new AtomicInteger(0);
 
     public static void main(String[] args) {
         // Создаем и запускаем три бд
@@ -39,21 +42,32 @@ public class App {
         }
 
 
-        for (int i = 0; i < 100; i++) {
-            String randomDbName = "db_" + ((int) (Math.random() * countDb) + 1);
+        for(int i = 1; i <= 3; i++){
+            int finalI = i;
+            thread = new Thread(() -> performQueries(String.valueOf(finalI)));
+            thread.start();
+        }
+
+
+
+    }
+
+    private static void performQueries(String threadName) {
+        while (counter.getAndIncrement() < 100) {
+            String randomDbName = "db_" + ((int) (Math.random() * 3) + 1);
             int randomValue = (int) (Math.random() * 1000) + 1;
-            queryRandomValue(randomDbName, randomValue);
+            queryRandomValue(randomDbName, randomValue, threadName);
         }
     }
 
-    private static void queryRandomValue(String dbName, int value) {
+    private static void queryRandomValue(String dbName, int value, String threadName) {
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName, username, password)) {
             String querySQL = "SELECT column_name FROM table_name WHERE column_name = " + value;
             try (Statement statement = connection.createStatement()) {
                 if (statement.executeQuery(querySQL).next()) {
-                    System.out.println("Значение " + value + " найдено в базе данных " + dbName);
+                    System.out.println("Значение " + value + " найдено в базе данных " + dbName + " выполнил поток № " + threadName);
                 } else {
-                    System.out.println("Значение " + value + " не найдено в базе данных " + dbName);
+                    System.out.println("Значение " + value + " не найдено в базе данных " + dbName + "поток № " + threadName);
                 }
             }
         } catch (SQLException e) {
